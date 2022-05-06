aa9f6547f99c2f9a39ea71043b2c9da84564a781
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-06 15:16:49 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import me.karunarathne.DesignPatterns.Memento.Editor;
 public class Main {
     public static void main(String[] args) {
         var editor = new Editor();
-        e
+        editor.setContent("a");
     }
 }
